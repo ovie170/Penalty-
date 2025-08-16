910b70ea7adf4a3588e4b86cbb49c06a79752e7c
ovie170 <164542915+ovie170@users.noreply.github.com>
2025-08-17 00:45:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+﻿// Wait 3 seconds then hide splash and show sign in
+setTimeout(() => {
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("signin").classList.remove("hidden");
+}, 3000);
---
+++ splash.js
@@ -0,0 +1,10 @@
+﻿// How long to show the splash (ms)
+const DURATION = 3000;
+
+// After DURATION, go to your next page
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    // Change this to "signin.html" once your sign-in page is ready
+    window.location.href = "signin.html";
+  }, DURATION);
+});
